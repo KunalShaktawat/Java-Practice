07e68445a834d1070c3062a97c06ec8254962d5b
Kunal Shaktawat <kunalshaktawat97@gmail.com>
2025-12-22 12:20:06 +0530

    Solved minimum sum of subarray

---
+++ Java-Practice/src/dsajava/MinSumSubarray.java
@@ -0,0 +1,19 @@
+package dsajava;
+
+public class MinSumSubarray {
+	public static void main(String[] args) {
+		int[] arr= {3,3,4,2,6,3,1,0};
+		int k=3;
+		int minSum=0;
+		for(int i=0;i<k;i++) {
+			minSum+=arr[i];
+		}
+		int curr_sum=minSum;
+		for(int j=k;j<arr.length;j++) {
+			curr_sum+=arr[j]-arr[j-k];
+			minSum=Math.min(minSum, curr_sum);
+		}
+		System.out.println("Minimum Sum: "+minSum);
+	}
+
+}
